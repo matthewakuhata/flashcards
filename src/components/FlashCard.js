f3c8f78204499ab1c9d5ce2d73d4bb331dbe38b0
Matthew Akuhata <matthewakuhata@gmail.com>
2022-10-18 17:52:21 +1100

    extract flash card into own component

---
+++ src/components/FlashCard.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const FlashCard = ({ kun, on, english, ordering, clue, notes }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [hidden, setHidden] = useState(false);
+  const [playAnimation, setPlayAnimation] = useState(false);
+
+  const onClickFlashCard = () => {
+    setPlayAnimation(true);
+
+    setTimeout(() => {
+      setHidden(true);
+      setShowAnswer(!showAnswer);
+    }, 200);
+
+    setTimeout(() => {
+      setPlayAnimation(false);
+      setHidden(false);
+    }, 400);
+  };
+  return (
+    <>
+      <div
+        className={`flashcard ${playAnimation ? " flip-vertical-left" : ""}`}
+        onClick={onClickFlashCard}
+      >
+        {!hidden && (
+          <>
+            <span>{ordering}</span>
+            {showAnswer ? (
+              <div className="flashcard__answer">
+                <p>Kun: {kun}</p>
+                <p>On: {on}</p>
+                <p>Translation: {english}</p>
+                {notes && <p>Notes: {notes}</p>}
+              </div>
+            ) : (
+              <div
+                className={`flashcard__clue ${
+                  clue.length > 6 ? "medium-text" : ""
+                }`}
+              >
+                {clue}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default FlashCard;
